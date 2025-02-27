93d811f6f1f68df4c4256086010e2cb5e0e062a7
sayseee <sayseee@gmail.com>
2025-02-27 17:59:37 +0300

    c

---
+++ scripts/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [ 
+  'recentform/first.html',
+  'recentform/icons/icon-192x192.png',
+  'recentform/icons/icon-512x512.png'
+  // Add other URLs you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
